3a3baff0efb9be1aa1bc2654f984d21f6c8c7908
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2023-07-19 10:48:34 +0900

    add BlogIndexPage

--- posts/first-blog-post.md
+++ posts/first-blog-post.md
@@ -1,6 +1,6 @@
 ---
 title: ブログ始めました
-published_at: 2023-07-19T03:29:000Z
+published_at: 2023-07-19T03:29:00.000Z
 snippet: 最初の記事
 ---
 
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,4 @@
-import { Head } from "$fresh/runtime.ts";
-import { Handlers } from "$fresh/server.ts";
-import { useSignal } from "@preact/signals";
+import { Handlers, PageProps } from "$fresh/server.ts";
 import { join } from "$std/path/mod.ts";
 import { extract } from "$std/front_matter/any.ts";
 
@@ -26,6 +24,9 @@ async function getPosts(): Promise<Post[]> {
     const slug = file.name.replace(".md", "");
     promises.push(getPost(slug));
   }
+  const posts = await Promise.all(promises) as Post[];
+  posts.sort((a, b) => b.publishedAt.getTime() - a.publishedAt.getTime());
+  return posts;
 }
 
 async function getPost(slug: string): Promise<Post | null> {
@@ -40,30 +41,37 @@ async function getPost(slug: string): Promise<Post | null> {
   };
 }
 
-export default function Home() {
-  const count = useSignal(3);
+function PostCard(props: { post: Post }) {
+  const { post } = props;
   return (
-    <>
-      <Head>
-        <title>blog-deno</title>
-      </Head>
-      <div class="px-4 py-8 mx-auto bg-[#86efac]">
-        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-          <img
-            class="my-6"
-            src="/logo.svg"
-            width="128"
-            height="128"
-            alt="the fresh logo: a sliced lemon dripping with juice"
-          />
-          <h1 class="text-4xl font-bold">Welcome to fresh</h1>
-          <p class="my-4">
-            Try updating this message in the
-            <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-          </p>
-          <Counter count={count} />
+    <div class="py-8 border(t gray-200)">
+      <a class="sm:col-span-2" href={`/${post.slug}`}>
+        <h3 class="text(3xl gray-900) font-bold">
+          {post.title}
+        </h3>
+        <time class="text-gray-500">
+          {new Date(post.publishedAt).toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </time>
+        <div class="mt-4 text-gray-900">
+          {post.snippet}
         </div>
+      </a>
+    </div>
+  );
+}
+
+export default function BlogIndexPage(props: PageProps<Post[]>) {
+  const posts = props.data;
+  return (
+    <main class="max-w-screen-md px-4 pt-16 mx-auto">
+      <h1 class="text-5xl font-bolf">Blog</h1>
+      <div class="mt-8">
+        {posts.map((p) => <PostCard post={p} />)}
       </div>
-    </>
+    </main>
   );
 }
