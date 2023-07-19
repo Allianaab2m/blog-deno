0903ec75e21f2c22d5bde80e4a89e255a7b52eae
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2023-07-19 22:03:49 +0900

    regenerate fresh.gen.ts

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,16 +5,14 @@
 import * as $0 from "./routes/[slug].tsx";
 import * as $1 from "./routes/_404.tsx";
 import * as $2 from "./routes/_app.tsx";
-import * as $3 from "./routes/greet/[name].tsx";
-import * as $4 from "./routes/index.tsx";
+import * as $3 from "./routes/index.tsx";
 
 const manifest = {
   routes: {
     "./routes/[slug].tsx": $0,
     "./routes/_404.tsx": $1,
     "./routes/_app.tsx": $2,
-    "./routes/greet/[name].tsx": $3,
-    "./routes/index.tsx": $4,
+    "./routes/index.tsx": $3,
   },
   islands: {},
   baseUrl: import.meta.url,
