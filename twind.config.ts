ee746968a2e4ccaa7159137b788ff45393200d31
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2023-07-20 17:29:56 +0900

    use IBM Plex Sans JP

--- twind.config.ts
+++ twind.config.ts
@@ -2,4 +2,15 @@ import { Options } from "$fresh/plugins/twind.ts";
 
 export default {
   selfURL: import.meta.url,
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: "IBM Plex Sans JP, sans-serif",
+      },
+    },
+  },
+  preflight: {
+    "@import":
+      `url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+JP:wght@100;200;300;400;500;600;700&display=swap')`,
+  },
 } as Options;
