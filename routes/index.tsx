cc2302a3c49a481b50e04d11cd20c4311fdf9dd6
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2023-08-05 04:36:47 +0900

    update

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,56 +1,11 @@
-import { Handlers, PageProps } from "$fresh/server.ts";
-import { getPost, Post } from "@/utils/posts.ts";
+import {Header} from "../components/Header.tsx";
+import {IconActivityPub} from "../components/IconActivityPub.tsx";
 
-export const handler: Handlers<Post[]> = {
-    async GET(_req, ctx) {
-        const posts = await getPosts();
-        return ctx.render(posts);
-    },
-};
-
-async function getPosts(): Promise<Post[]> {
-    const files = Deno.readDir("./posts");
-    const promises = [];
-    for await (const file of files) {
-        const slug = file.name.replace(".md", "");
-        promises.push(getPost(slug));
-    }
-    const posts = await Promise.all(promises) as Post[];
-    posts.sort((a, b) => b.publishedAt.getTime() - a.publishedAt.getTime());
-    return posts;
-}
-
-function PostCard(props: { post: Post }) {
-    const { post } = props;
+export default function IndexPage() {
     return (
-        <div class="py-8 border(t gray-200)">
-            <a class="sm:col-span-2" href={`/${post.slug}`}>
-                <h3 class="text(3xl gray-900) font-bold">
-                    {post.title}
-                </h3>
-                <time class="text-gray-500">
-                    {new Date(post.publishedAt).toLocaleDateString("en-US", {
-                        year: "numeric",
-                        month: "long",
-                        day: "numeric",
-                    })}
-                </time>
-                <div class="mt-4 text-gray-900">
-                    {post.snippet}
-                </div>
-            </a>
-        </div>
-    );
-}
-
-export default function BlogIndexPage(props: PageProps<Post[]>) {
-    const posts = props.data;
-    return (
-        <main class="max-w-screen-md px-4 pt-16 mx-auto">
-            <h1 class="text-5xl font-bolf">ありあなのBlog</h1>
-            <div class="mt-8">
-                {posts.map((p) => <PostCard post={p} />)}
-            </div>
+        <main className="max-w-screen-md px-4 pt-16 mx-auto">
+            <Header />
+            <h1 className="text-5xl font-bolf">ありあなのホームページ</h1>
         </main>
     );
 }
